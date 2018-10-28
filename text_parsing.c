0417cb6cc6b6a6baf9b62a40a5d76f79776d1104
Patrick Lown <lown@snares-02.cs.wisc.edu>
2018-10-28 17:24:04 -0500

    testing parser

--- text_parsing.c
+++ text_parsing.c
@@ -1,35 +1,64 @@
-#include text_parsing.h
+#include "text_parsing.h"
+#include <stdio.h>
+#include <string.h>
+
+
+// target** parseFile(char * filename){
+// 	FILE *fp;
+// 	int endoffile;
+
+// 	fp = fopen(filename, "r");
+// 	if(fp == NULL){
+// 		fprintf(stderr,"Cannot open file %s/n",filename);
+// 		return NULL;
+// 	}
+// 	endoffile = 0;
+// 	while(!endoffile){
+// 		buff = malloc(buffer_init);
+// 		char* c = fgetc(fp);
+// 		get_next_target(c,fp) 
+
+
+// 	}
+// 	return NULL;
+// }
+
+// /*
 
 
 
-target** parseFile(char * filename){
-	FILE *fp;
 
-	fp = fopen(filename, "r");
-	if(fp == NULL){
-		fprintf(stderr,"Cannot open file %s/n",filename);
-		return NULL;
+// */
+// char* get_next_target(char* c,FILE* fp){
+// 	while(c == "\n" || c != EOF){
+// 		c = fgetc(fp);
+// 	}
+// 	return NULL;
+// }
+
+char* create_target(char* line)
+{
+	const char* s = ":";
+	char* tok;
+	tok = strtok(line,s);
+
+	while(tok != 0){
+		printf("%s\n",tok);
+		tok = strtok(0,s);
 	}
-	endoffile = 0;
-	while(!endoffile){
-		buff = malloc(buffer_init);
-		char* c = fgetc(fp);
-		get_next_target(c,fp) 
 
-
+	return line;
 }
-/*
 
+int main(){
+	char x[] = "537ps:  537ps.o readproc.o parseopts.o output.o";
+	const char* s = ":";
+	char* tok;
+	tok = strtok(x,s);
 
-
-
-*/
-char* get_next_target(char* c,FILE* fp){
-	while(c == "\n" || c != EOF){
-		c = fgetc(fp);
+	while(tok != 0){
+		printf("%s",tok);
+		tok = strtok(0,s);
 	}
-}
-int check_valid_target(char* c){
-
 
 }
--- text_parsing.h
+++ text_parsing.h
@@ -5,16 +5,18 @@ const int buffer_limit = 1024;
 const int buffer_init = 64;
 
 
-typedef struct{
-	target** targets;
-	int size;
-}target_list;
 
-typdef struct{
+
+typedef struct target{
 	char* name;
 	char** dependecies;
 	char** cmds;
 }target;
+
+typedef struct target_list{
+	target** targets;
+	int size;
+}target_list;
 
 
 
