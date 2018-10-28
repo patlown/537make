e25c419037b94cb6c5309a389672bb9c549d6b38
Ge Xu <ge@snares-01.cs.wisc.edu>
2018-10-28 17:22:27 -0500

    modified parser

--- text_parsing.c
+++ text_parsing.c
@@ -1,6 +1,8 @@
-#include text_parsing.h
+#include "text_parsing.h"
 
-
+char* double_buff(char* buff, int cursize);
+void skip_empty(int* c,FILE* fp);
+char* read_line(int* c, FILE* fp);
 
 target** parseFile(char * filename){
 	FILE *fp;
@@ -10,13 +12,12 @@ target** parseFile(char * filename){
 		fprintf(stderr,"Cannot open file %s/n",filename);
 		return NULL;
 	}
-	endoffile = 0;
-	while(!endoffile){
-		buff = malloc(buffer_init);
-		char* c = fgetc(fp);
-		get_next_target(c,fp) 
-
-
+	// endoffile = 0;
+	// while(!endoffile){
+	// 	buff = malloc(buffer_init);
+	int c = fgetc(fp);
+	// 	get_next_target(c,fp) 
+	return NULL;
 }
 /*
 
@@ -24,12 +25,65 @@ target** parseFile(char * filename){
 
 
 */
-char* get_next_target(char* c,FILE* fp){
-	while(c == "\n" || c != EOF){
-		c = fgetc(fp);
+void skip_empty(int* c,FILE* fp){
+	while(*c == '\n' || *c != EOF){
+		*c = fgetc(fp);
 	}
 }
-int check_valid_target(char* c){
 
+// target* get_target(char* c){
+	
+// }
 
+char* read_line(int* c, FILE* fp){
+	char* buff = malloc(buffer_init);
+	int index = 0,cur_size = buffer_init;
+
+	if(buff == NULL){
+    		fprintf(stderr, "Memory Allocation failure of Buffer\n");
+        	return NULL;
+    }
+	while(*c!='\n' && *c!=EOF ){
+		if(index == cur_size){
+			char* buff_new = double_buff(buff,cur_size);
+			if(!buff_new){
+				return NULL;
+			}else{
+				free(buff);
+				buff = buff_new;
+				cur_size*=2;
+			}
+		}
+		buff[index++] = (char)*c;
+		*c = fgetc(fp);
+	}
+	if(index == cur_size){
+			char* buff_new = double_buff(buff,cur_size);
+			if(!buff_new){
+				return NULL;
+			}else{
+				free(buff);
+				buff = buff_new;
+				cur_size*=2;
+			}
+	}
+	buff[index] = '\n';
+	return buff;
+}
+
+char* double_buff(char* buff, int cursize){
+	if(cursize == buffer_limit){
+				fprintf(stderr, "Line exceed max line limit!!\n");
+       			return NULL;
+	}
+			
+	char* buff_double = malloc(cursize*2);
+	if(buff_double == NULL){
+    	fprintf(stderr, "Memory Allocation failure of Buffer\n");
+        return NULL;
+    }
+	for(int i = 0; i < cursize; i++){
+		buff_double[i] = buff[i];
+	}
+	return buff_double;
 }
--- text_parsing.h
+++ text_parsing.h
@@ -1,20 +1,23 @@
 #ifndef TEXT_PARSING_H
 #define TEXT_PARSING_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 const int buffer_limit = 1024;
 const int buffer_init = 64;
 
+typedef struct{
+	char* name;
+	char** dependecies;
+	char** cmds;
+}target;
 
 typedef struct{
 	target** targets;
 	int size;
 }target_list;
 
-typdef struct{
-	char* name;
-	char** dependecies;
-	char** cmds;
-}target;
+
 
 
 
