985b235e0453f12aedaa6121cdae6a132fe8c600
Pat Lown <lown@wisc.edu>
2018-10-29 19:32:09 -0500

    trying to fix include errors

--- build_spec_graph.h
+++ build_spec_graph.h
@@ -1,5 +1,5 @@
-#ifndef BUILD_SPEC_GRAPH_H
-#define BUILD_SPEC_GRAPH_H
+#ifndef BUILD_SPEC_GRAPH
+#define BUILD_SPEC_GRAPH
 
 #include "text_parsing.h"
 
--- text_parsing.h
+++ text_parsing.h
@@ -1,11 +1,15 @@
-#ifndef TEXT_PARSING_H
-#define TEXT_PARSING_H
+#ifndef TEXT_PARSING
+#define TEXT_PARSING
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 const int buffer_limit = 1024;
 const int buffer_init = 64;
+
 int num_targets;
+
 typedef struct list_node list_node;
 typedef struct target target;
 typedef struct target_node target_node;
