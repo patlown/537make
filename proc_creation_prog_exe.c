f94d34160d53f8395eb087bf9b965415d343d502
Patrick Lown <lown@snares-05.cs.wisc.edu>
2018-11-03 17:33:43 -0500

    started working on process creation

---
+++ proc_creation_prog_exe.c
@@ -0,0 +1,43 @@
+#include "build_spec_graph.h"
+#include "text_parsing.h"
+
+/* This function checks if the node passed in exists as a target, prints an error to stderr if it does not 
+and returns null.  Once the node is found, it calls the run commands function which runs commands bottom-up from the subgraph of
+current node
+*/
+void makeTargets(char* name, graph_node_list* gnl){
+    graph_node* curr_graph_node = NULL;
+
+    //find the addr of the node we need to build
+    while(gnl != NULL){
+        //check for matching name
+        if(strcmp(gnl->name,name) == 0){
+            //check that it is a target, ie, the matching node contains a target node
+            if(gnl->addr->gnt != NULL){
+                curr_graph_node = gnl->addr;
+                //once we've met these conditions, we break
+                break;
+            }  
+        }
+    }
+    //if curr_graph_node stays null, then we never found the target in our graph_node_list
+    if(curr_graph_node == NULL){
+        fprintf(stderr,"%s is not a valid target in this makefile",name);
+        return NULL;
+    }
+
+
+}
+
+void runCommands(graph_node* node){
+    //check if nodes have children, for each child, recurse
+    if(node->gnt->deps_size > 0){
+        for(int i = 0; i < deps_size; i++){
+            runCommands(node->children[i]);
+        }
+    }else{
+        
+    }
+
+
+}
