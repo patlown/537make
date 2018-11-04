9e2dce6512820e602d7491785a6cee75702eab00
Patrick Lown <lown@snares-05.cs.wisc.edu>
2018-11-03 19:57:40 -0500

    added execvp link to main

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #include "build_spec_graph.h"
+#include <unistd.h>
+
 
 int main(int argc, char** argv){
 	target_node *t = parseFile(argv[1]);
@@ -16,15 +18,19 @@ int main(int argc, char** argv){
 	// 	printf("%s\n",ptrlist->name);
 	// 	ptrlist = ptrlist->next;
 	// }
-	graph_node_list* curr_gnle = list;
-	while(curr_gnle != NULL){
-		if(curr_gnle->addr->children != NULL){
-			printf("This Node has children: %s\n",curr_gnle->name);
-			for(int i = 0; i < curr_gnle->addr->gnt->deps_size; i++){
-				printf("Child %d: %s\n",i,curr_gnle->addr->children[i]->name);
-			 }
-		}
-		curr_gnle = curr_gnle->next;
-	}
+	// graph_node_list* curr_gnle = list;
+	// while(curr_gnle != NULL){
+	// 	if(curr_gnle->addr->children != NULL){
+	// 		printf("This Node has children: %s\n",curr_gnle->name);
+	// 		for(int i = 0; i < curr_gnle->addr->gnt->deps_size; i++){
+	// 			printf("Child %d: %s\n",i,curr_gnle->addr->children[i]->name);
+	// 		 }
+	// 	}
+	// 	curr_gnle = curr_gnle->next;
+	// }
+
+	char* cmd = "gcc main.c build_spec_graph.c";
+	execvp(cmd);
+	//check out this link: http://www.csl.mtu.edu/cs4411.ck/www/NOTES/process/fork/exec.html
 	return 0;
 }
